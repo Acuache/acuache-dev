640632ed0316a72b73f78ec32bbd632cd9722ec4
AcuacheY <v.acuache15@gmail.com>
2025-11-08 18:36:30 -0500

    feat: Enhance layout with max-width styling and add icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,18 @@
+export { default as Astroo } from './Astroo.astro';
+export { default as CSS } from './CSS.astro';
+export { default as Figma } from './Figma.astro';
+export { default as Git } from './Git.astro';
+export { default as Github } from './Github.astro';
+export { default as HTML } from './HTML.astro';
+export { default as JavaScript } from './JavaScript.astro';
+export { default as LinkedIn } from './LinkedIn.astro';
+export { default as PostgreSQL } from './PostgreSQL.astro';
+export { default as React } from './React.astro';
+export { default as ReactRouter } from './ReactRouter.astro';
+export { default as Supabase } from './Supabase.astro';
+export { default as TailwindCSS } from './TailwindCSS.astro';
+export { default as TanStack } from './TanStack.astro';
+export { default as TypeScript } from './TypeScript.astro';
+export { default as Zod } from './Zod.astro';
+export { default as Zustand } from './Zustand.astro';
+
